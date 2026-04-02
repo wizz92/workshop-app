b7e3981a6598dc13e047746a522e865741f8cdd6
GOD <eligant4710@gmail.com>
2026-04-02 09:57:53 +0300

    test

--- src/screens/home.js
+++ src/screens/home.js
@@ -18,7 +18,7 @@ export function renderHome() {
   const heroHtml = hero
     ? `
       <section class="hero-section screen-enter" aria-labelledby="hero-title">
-        <p class="hero-section__eyebrow">Найближча подія</p>
+        <p class="hero-section__eyebrow">Найближча подія 1111213123</p>
         ${renderEventCard(hero, { variant: 'hero' })}
       </section>
     `
